9b16e868087e0f37fefae84839224a065902a50b
Alek <alekaker@outlook.com>
2019-03-17 16:28:56 -0600

    adding JS functionality

---
+++ main.js
@@ -0,0 +1,8 @@
+// var input = document.querySelector(".button-input");
+// var button = document.querySelector(".name-this-dog-button");
+// var span = document.querySelector("span");
+
+function myFunction() {
+ var input = document.getElementById("userInput").value;
+  document.getElementById("span-id").innerHTML=input;
+}
